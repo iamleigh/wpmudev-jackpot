c5ad7cd3b6ea3752c5bea0ea44db1d86f7015d86
Leighton Sapir <2328848+iamleigh@users.noreply.github.com>
2020-03-28 03:06:36 -0500

    :package: WPMU DEV Jackpot v1.0.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,41 +5,34 @@
 	var btnStop = document.querySelector('#machine-stop');
 	var casino1 = document.querySelector('#casino1');
 	var casino2 = document.querySelector('#casino2');
-	var casino3 = document.querySelector('#casino3');
 	var mCasino1 = new SlotMachine(casino1, {
-	active: 0,
-	delay: 500
+		active: 0,
+		delay: 500
 	});
 	var mCasino2 = new SlotMachine(casino2, {
-	active: 1,
-	delay: 500
-	});
-	var mCasino3 = new SlotMachine(casino3, {
-	active: 2,
-	delay: 500
+		active: 1,
+		delay: 500
 	});
 
 	btnShuffle.addEventListener('click', function() {
-		count = 3;
+		count = 2;
+		btnShuffle.setAttribute( 'disabled', '' );
 		mCasino1.shuffle(9999);
 		mCasino2.shuffle(9999);
-		mCasino3.shuffle(9999);
-		btnShuffle.setAttribute( 'disabled', '' );
 		btnStop.removeAttribute( 'disabled' );
 	  });
 
 	  btnStop.addEventListener('click', function() {
 		switch(count) {
-		  case 3:
+		  case 2:
 			mCasino1.stop();
 			break;
-		  case 2:
-			mCasino2.stop();
-			break;
 		  case 1:
-			mCasino3.stop();
+			mCasino2.stop();
 			btnStop.setAttribute( 'disabled', '' );
-			btnShuffle.removeAttribute( 'disabled' );
+			setTimeout( function() {
+				btnShuffle.removeAttribute( 'disabled' );
+			}, 10000 );
 			break;
 		}
 		count--;
